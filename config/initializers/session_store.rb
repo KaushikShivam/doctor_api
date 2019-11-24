4ad72138be9c30e4ab771693fded13b0680d76d4
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-11-25 00:32:27 +0530

    Configure rails app to work with localhost

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,5 @@
-Rails.application.config.session_store :cookie_store, key: "_doctor_api", domain: "doctor_api.herokuapp.com"
+if Rails.env.production?
+  Rails.application.config.session_store :cookie_store, key: "_doctor_api", domain: "doctor_api.herokuapp.com"
+else
+  Rails.application.config.session_store :cookie_store, key: "_doctor_api"
+end

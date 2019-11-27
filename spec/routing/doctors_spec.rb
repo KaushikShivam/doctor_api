0f46a6eebf9da5bc21d89d8e69ec136f9d657070
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-11-28 01:54:36 +0530

    Comment out alternate index

--- spec/routing/doctors_spec.rb
+++ spec/routing/doctors_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 describe 'doctors router' do
+  # it "should route to doctors index" do
+  #   expect(get '/doctors').to route_to('doctors#index')
+  # end
+  # commented this to use named path. Keeping it here for ref
   it "should route to doctors index" do
     expect(get doctors_path).to route_to('doctors#index')
-  end
-  
-  it "should route to doctors index" do
-    expect(get '/doctors').to route_to('doctors#index')
   end
 end

741ab39944d714a05b1b2c72b3b129f555915adc
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-11-28 00:46:24 +0530

    Add basic validations to pass the tests

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,2 +1,10 @@
 class Doctor < ApplicationRecord
+  validates :name, presence: true
+  validates :category, presence: true
+  validates :description, presence: true
+  validates :fee, presence: true
+  validates :exp, presence: true
+  validates :likes, presence: true
+  validates :phone, presence: true
+  validates :address, presence: true
 end
--- spec/models/doctor_spec.rb
+++ spec/models/doctor_spec.rb
@@ -8,49 +8,49 @@ RSpec.describe Doctor, type: :model do
   it 'should validate the presence of name' do
     doctor = FactoryBot.build(:doctor, name: '')
     expect(doctor).not_to be_valid
-    expect(doctor.error.messages[:name]).to include("can't be blank")
+    expect(doctor.errors.messages[:name]).to include("can't be blank")
   end
   
   it 'should validate the presence of category' do
     doctor = FactoryBot.build(:doctor, category: '')
     expect(doctor).not_to be_valid
-    expect(doctor.error.messages[:category]).to include("can't be blank")
+    expect(doctor.errors.messages[:category]).to include("can't be blank")
   end
   
   it 'should validate the presence of description' do
     doctor = FactoryBot.build(:doctor, description: '')
     expect(doctor).not_to be_valid
-    expect(doctor.error.messages[:description]).to include("can't be blank")
+    expect(doctor.errors.messages[:description]).to include("can't be blank")
   end
   
   it 'should validate the presence of fee' do
     doctor = FactoryBot.build(:doctor, fee: '')
     expect(doctor).not_to be_valid
-    expect(doctor.error.messages[:fee]).to include("can't be blank")
+    expect(doctor.errors.messages[:fee]).to include("can't be blank")
   end
   
   it 'should validate the presence of exp' do
     doctor = FactoryBot.build(:doctor, exp: '')
     expect(doctor).not_to be_valid
-    expect(doctor.error.messages[:exp]).to include("can't be blank")
+    expect(doctor.errors.messages[:exp]).to include("can't be blank")
   end
   
   it 'should validate the presence of likes' do
     doctor = FactoryBot.build(:doctor, likes: '')
     expect(doctor).not_to be_valid
-    expect(doctor.error.messages[:likes]).to include("can't be blank")
+    expect(doctor.errors.messages[:likes]).to include("can't be blank")
   end
   
   it 'should validate the presence of phone' do
     doctor = FactoryBot.build(:doctor, phone: '')
     expect(doctor).not_to be_valid
-    expect(doctor.error.messages[:phone]).to include("can't be blank")
+    expect(doctor.errors.messages[:phone]).to include("can't be blank")
   end
   
   it 'should validate the presence of address' do
     doctor = FactoryBot.build(:doctor, address: '')
     expect(doctor).not_to be_valid
-    expect(doctor.error.messages[:address]).to include("can't be blank")
+    expect(doctor.errors.messages[:address]).to include("can't be blank")
   end
   
 end

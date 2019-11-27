77e3af7104702019c61428f3f9fb397656a1ecb5
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-11-28 00:06:00 +0530

    Add base doctor model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_24_171116) do
+ActiveRecord::Schema.define(version: 2019_11_27_183510) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "doctors", force: :cascade do |t|
+    t.string "name"
+    t.string "category"
+    t.text "description"
+    t.string "fee"
+    t.string "exp"
+    t.string "likes"
+    t.string "phone"
+    t.string "address"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
---
+++ spec/factories/doctors.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :doctor do
+    name { "MyString" }
+    category { "MyString" }
+    description { "MyText" }
+    fee { "MyString" }
+    exp { "MyString" }
+    likes { "MyString" }
+    phone { "MyString" }
+    address { "MyString" }
+  end
+end

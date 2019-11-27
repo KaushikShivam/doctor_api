1b0c801e8cccf22787ad0bc70c1eb19965e1ba75
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-11-28 02:27:54 +0530

    Add test for proper json from Article index

--- spec/controllers/doctors_controller_spec.rb
+++ spec/controllers/doctors_controller_spec.rb
@@ -6,5 +6,13 @@ describe DoctorsController do
       get :index
       expect(response).to have_http_status(:ok)
     end
+    
+    it 'should return proper json' do
+      get :index
+      json = JSON.parse(response.body)
+      json_data = json[:data]
+      expect(json_data.length).to eq(2)
+    end
+    
   end
 end

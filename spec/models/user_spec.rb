94d5e61db269602c922ae6761b49e1dd651adf94
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-11-28 16:13:39 +0530

    Add passing tests for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,15 +9,15 @@ RSpec.describe User, type: :model do
     
     it 'should validate presence of attributes' do
       user = build :user, name: nil, email: ''
-      expect(user).to not_be_valid
-      expect(user.error.messages[:name]).to include("can't be blank")
-      expect(user.error.messages[:email]).to include("can't be blank")
+      expect(user).not_to be_valid
+      expect(user.errors.messages[:name]).to include("can't be blank")
+      expect(user.errors.messages[:email]).to include("can't be blank")
     end
     
     it 'should check uniqueness of the email' do
       user = create :user
-      other_user = create :user, email: user.email
-      expect(other_user).not_to be valid
+      other_user = build :user, email: user.email
+      expect(other_user).not_to be_valid
       other_user.email = 'newlogin@gmail.com'
       expect(other_user).to be_valid
     end

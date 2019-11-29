f6db47aa337b97e09aa3de99ba22eaaa1d32a036
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-11-30 02:55:52 +0530

    Finish create doctors controller

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -10,5 +10,21 @@ class AppointmentsController < ApplicationController
   end
   
   def create
+    if @current_user
+      appointment = @current_user.appointments.build(appointment_params)
+      if appointment.save
+        render json: appointment, status: :ok
+      end
+    else
+      render json: doctor, serializer: ActiveModel::Serializer::ErrorSerializer, status: :unprocessable_entity
+    end
   end
+  
+  private
+  def appointment_params
+    params.require(:data).require(:attributes)
+      .permit(:patient, :reason, :date, :time, :doctor_id) ||
+      ActionController::Parameters.new
+  end
+  
 end

c2140c6cdb1c8f520bb5d570ad9814b6ab3e7514
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-12-01 20:12:31 +0530

    Add production url for CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -16,7 +16,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
   
   allow do
-    origins 'https://doctor-react.herokuapp.com'
+    origins 'https://doctorly-official.herokuapp.com/'
 
     resource '*',
       headers: :any,
